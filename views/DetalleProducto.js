12fa9c24e75f1dc7147b51a0d76a84b24f68b28d
lionezel <lionezelfts@gmail.com>
2023-09-07 18:05:59 -0500

    mostrando los detalles del producto

--- views/DetalleProducto.js
+++ views/DetalleProducto.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text } from 'react-native'
+import { PedidoContext } from "../context/pedidos/pedidosContext";
+import { Container, Image } from "native-base";
+import { globalStyles } from "../styles/global";
 
 export default function DetalleProducto() {
-  return <Text>DetalleProducto</Text>;
+
+  //Context de producto
+  const { producto } = useContext(PedidoContext)
+  const { nombre, imagen, descripcion, precio } = producto
+
+  return( 
+    <Container style={globalStyles.contenedor}>
+      <Text>{nombre}</Text>
+      <Image  size={600} source={{ uri: imagen }}/>
+    </Container>
+    
+  )
 }
