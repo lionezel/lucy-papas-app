0b1e5742186b37cfdc0a159c4d512b2189409126
lionezel <lionezelfts@gmail.com>
2023-09-07 18:27:18 -0500

    boton de cantidad

--- views/FormularioProducto.js
+++ views/FormularioProducto.js
@@ -1,9 +1,17 @@
+import { Button, Container, HStack, Icon, InputRightAddon } from 'native-base'
 import React from 'react'
 import { Text } from 'react-native'
 
 
 export default function FormularioProducto() {
   return (
-    <Text>FormularioProducto</Text>
+    <Container>
+      <Text>Cantidad</Text>
+      <HStack>
+        <Button><Icon name='remove'/></Button>
+        <InputRightAddon>1</InputRightAddon>
+        <Button><Icon name='remove'/></Button>
+      </HStack>
+    </Container>
   )
 }
