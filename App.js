f1e41e3fc0c364ef710af768c4d0fbaf9ab68ee4
lionezel <lionezelfts@gmail.com>
2023-09-07 10:13:45 -0500

    creando las otras vistas faltantes

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import 'react-native-gesture-handler';
-import React from 'react';
+import "react-native-gesture-handler";
+import React from "react";
 
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack';
-import NuevoProducto from './views/NuevoProducto';
-
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import NuevoProducto from "./views/NuevoProducto";
+import Menu from "./views/Menu";
+import DetalleProducto from "./views/DetalleProducto";
+import FormularioProducto from "./views/FormularioProducto";
+import ResumenPedido from "./views/ResumenPedido";
+import { ProgresoPedido } from "./views/ProgresoPedido";
 
 const Stack = createStackNavigator();
 
@@ -13,12 +16,56 @@ export default function App() {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen 
-            name='Nueva Orden'
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#FFDA00",
+            },
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        >
+          <Stack.Screen
+            name="Nueva Orden"
             component={NuevoProducto}
             options={{
-              title: "Nueva Orden"
+              title: "Nueva Orden",
+            }}
+          />
+          <Stack.Screen
+            name="Menu"
+            component={Menu}
+            options={{
+              title: "Nuestro menu",
+            }}
+          />
+          <Stack.Screen
+            name="DetalleProducto"
+            component={DetalleProducto}
+            options={{
+              title: "Detalle producto",
+            }}
+          />
+          <Stack.Screen
+            name="FormularioProducto"
+            component={FormularioProducto}
+            options={{
+              title: "Ordenar producto",
+            }}
+          />
+          <Stack.Screen
+            name="ResumenPedido"
+            component={ResumenPedido}
+            options={{
+              title: "Resumen pedido",
+            }}
+          />
+          <Stack.Screen
+            name="ProgresoPedido"
+            component={ProgresoPedido}
+            options={{
+              title: "Progreso del pedido",
             }}
           />
         </Stack.Navigator>
@@ -26,4 +73,3 @@ export default function App() {
     </>
   );
 }
-
