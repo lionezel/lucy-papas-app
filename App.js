24649efd209aa892ae42259b9278da05984b4a6f
lionezel <lionezelfts@gmail.com>
2023-09-07 10:46:19 -0500

    consumiendo los satet

--- App.js
+++ App.js
@@ -13,68 +13,71 @@ import { ProgresoPedido } from "./views/ProgresoPedido";
 
 //Importar el state de context
 import { FirebaseState } from "./context/firebase/firebaseState";
+import { PedidosState } from "./context/pedidos/pedidosState";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-    <FirebaseState>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: "#FFDA00",
-            },
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
-        >
-          <Stack.Screen
-            name="Nueva Orden"
-            component={NuevoProducto}
-            options={{
-              title: "Nueva Orden",
-            }}
-          />
-          <Stack.Screen
-            name="Menu"
-            component={Menu}
-            options={{
-              title: "Nuestro menu",
-            }}
-          />
-          <Stack.Screen
-            name="DetalleProducto"
-            component={DetalleProducto}
-            options={{
-              title: "Detalle producto",
-            }}
-          />
-          <Stack.Screen
-            name="FormularioProducto"
-            component={FormularioProducto}
-            options={{
-              title: "Ordenar producto",
-            }}
-          />
-          <Stack.Screen
-            name="ResumenPedido"
-            component={ResumenPedido}
-            options={{
-              title: "Resumen pedido",
-            }}
-          />
-          <Stack.Screen
-            name="ProgresoPedido"
-            component={ProgresoPedido}
-            options={{
-              title: "Progreso del pedido",
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <FirebaseState>
+        <PedidosState>
+          <NavigationContainer>
+            <Stack.Navigator
+              screenOptions={{
+                headerStyle: {
+                  backgroundColor: "#FFDA00",
+                },
+                headerTitleStyle: {
+                  fontWeight: "bold",
+                },
+              }}
+            >
+              <Stack.Screen
+                name="Nueva Orden"
+                component={NuevoProducto}
+                options={{
+                  title: "Nueva Orden",
+                }}
+              />
+              <Stack.Screen
+                name="Menu"
+                component={Menu}
+                options={{
+                  title: "Nuestro menu",
+                }}
+              />
+              <Stack.Screen
+                name="DetalleProducto"
+                component={DetalleProducto}
+                options={{
+                  title: "Detalle producto",
+                }}
+              />
+              <Stack.Screen
+                name="FormularioProducto"
+                component={FormularioProducto}
+                options={{
+                  title: "Ordenar producto",
+                }}
+              />
+              <Stack.Screen
+                name="ResumenPedido"
+                component={ResumenPedido}
+                options={{
+                  title: "Resumen pedido",
+                }}
+              />
+              <Stack.Screen
+                name="ProgresoPedido"
+                component={ProgresoPedido}
+                options={{
+                  title: "Progreso del pedido",
+                }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </PedidosState>
       </FirebaseState>
     </>
   );
---
+++ context/pedidos/pedidosState.js
@@ -0,0 +1,22 @@
+import React, { useReducer } from "react";
+
+import PedidoContext from "./pedidosContext";
+import PedidosReducer from "./pedidosReducer";
+
+export const PedidosState = (props) => {
+
+
+    //Crear state inicial
+    const initialState = {
+        pedido: []
+    }
+
+    //useReducer con dispatch para ejecutar las funciones
+    const [state, dispatch] = useReducer(PedidosReducer, initialState)
+
+  return <PedidosReducer.Provider
+    value={{
+        pedido: state.pedido
+    }}
+  >{props.children}</PedidosReducer.Provider>;
+};
