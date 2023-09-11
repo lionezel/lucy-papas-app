ff30e104762204fc079437ba496c33deb5c5bd14
lionezel <lionezelfts@gmail.com>
2023-09-11 12:49:54 -0500

    marcar ordenes como listas

--- views/ProgresoPedido.js
+++ views/ProgresoPedido.js
@@ -2,32 +2,36 @@ import React, { useContext, useEffect, useState } from "react";
 import { Text } from "react-native";
 import { PedidoContext } from "../context/pedidos/pedidosContext";
 import { firebase } from "../firebase/firebase";
-import { Container, View } from "native-base";
+import { Button, Container, View } from "native-base";
 import Countdown from "react-countdown";
 
 export const ProgresoPedido = () => {
   const { idpedido } = useContext(PedidoContext);
 
   const [tiempo, setTiempo] = useState(0);
+  const [completado, setCompletado] = useState(false);
 
   useEffect(() => {
     const obtenerProducto = () => {
       firebase.db
-        .collection('ordenes')
+        .collection("ordenes")
         .doc(idpedido)
         .onSnapshot(function (doc) {
           setTiempo(doc.data().tiempoentrega);
+          setCompletado(doc.data().completado);
         });
     };
-    obtenerProducto()
+    obtenerProducto();
   }, []);
 
-  //Muestra el countdown en la pantalla 
+  //Muestra el countdown en la pantalla
   const renderer = ({ minutes, seconds }) => {
     return (
-      <Text>{minutes}:{seconds}</Text>
-    )
-  }
+      <Text>
+        {minutes}:{seconds}
+      </Text>
+    );
+  };
 
   return (
     <Container>
@@ -39,20 +43,28 @@ export const ProgresoPedido = () => {
           </>
         )}
 
-          {
-            tiempo > 0 && (
-              <>
-              <Text>Su orden esta lista en: </Text>
-              <Text>
-                <Countdown 
-                  date={Date.now() + tiempo * 60000}
-                  renderer={renderer}
-                />
-              </Text>
-              </>
-            )
-          }
+        {!completado && tiempo > 0 && (
+          <>
+            <Text>Su orden esta lista en: </Text>
+            <Text>
+              <Countdown
+                date={Date.now() + tiempo * 60000}
+                renderer={renderer}
+              />
+            </Text>
+          </>
+        )}
 
+        {completado && (
+          <>
+            <Text>Orden lista</Text>
+            <Text>Por favor, pase a recoger su pedido</Text>
+
+            <Button>
+              <Text>Comenzar una orden nueva</Text>
+            </Button>
+          </>
+        )}
       </View>
     </Container>
   );
