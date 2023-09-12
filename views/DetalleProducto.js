467f31d16d9cc4ce9629e0b38d19c15d5ed06c0a
lionezel <lionezelfts@gmail.com>
2023-09-12 10:31:54 -0500

    estilos de detalle de cada producto

--- views/DetalleProducto.js
+++ views/DetalleProducto.js
@@ -1,7 +1,20 @@
 import React, { useContext } from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { PedidoContext } from "../context/pedidos/pedidosContext";
-import { Button, Container, Image } from "native-base";
+import {
+  AspectRatio,
+  Box,
+  Button,
+  Card,
+  Center,
+  Container,
+  HStack,
+  Heading,
+  Image,
+  Stack,
+  VStack,
+  View,
+} from "native-base";
 import { globalStyles } from "../styles/global";
 import { useNavigation } from "@react-navigation/native";
 
@@ -14,15 +27,50 @@ export default function DetalleProducto() {
   const navigation = useNavigation();
 
   return (
-    <Container style={globalStyles.contenedor}>
-      <Text>{nombre}</Text>
-      <Image size={200} source={{ uri: imagen }} alt={nombre} />
-      <Button
-        style={globalStyles.boton}
-        onPress={() => navigation.navigate("FormularioProducto")}
-      >
-        <Text style={globalStyles.botonTexto}>Ordenar producto</Text>
-      </Button>
-    </Container>
+    <>
+      <Container style={[globalStyles.contenedor, globalStyles.contenido, estilosProducto.contenedor]}>
+        <VStack space={1} alignItems="center">
+          <Text style={estilosProducto.titulo}>{nombre}</Text>
+          <Card style={estilosProducto.card}>
+            <Image size={300} source={{ uri: imagen }} alt={nombre} />
+
+            <Text style={estilosProducto.textos}>{descripcion}</Text>
+            <Text style={[estilosProducto.textos, estilosProducto.precio]}>Precio: ${precio}</Text>
+          </Card>
+        </VStack>
+        <Button
+          style={[globalStyles.boton, estilosProducto.boton_ordenar]}
+          onPress={() => navigation.navigate("FormularioProducto")}
+        >
+          <Text style={globalStyles.botonTexto}>Ordenar producto</Text>
+        </Button>
+      </Container>
+    </>
   );
 }
+
+const estilosProducto = StyleSheet.create({
+  contenedor: {
+    alignItems: "center"
+  },
+
+  titulo: {
+    fontSize: 30,
+    marginTop: 20,
+    marginBottom: 20
+  },
+
+  textos: {
+    marginTop: 20,
+    fontSize: 20
+  },
+
+  precio: {
+    fontWeight: "bold"
+  },
+  boton_ordenar: {
+    marginTop: "30%",
+    width: "100%",
+    height: "7%",
+  },
+});
